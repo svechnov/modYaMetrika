907a9f4f0b46606be6f00801e844f1eee250752a
Evgeniy <sei.olympus@gmail.com>
2014-10-26 01:27:01 +0600

    req #3

--- assets/components/modyametrika/connector.php
+++ assets/components/modyametrika/connector.php
@@ -1,7 +1,7 @@
 <?php
 // For debug
-ini_set('display_errors', 1);
-ini_set('error_reporting', -1);
+ ini_set('display_errors', 1);
+ ini_set('error_reporting', -1);
 
 // require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php')) {
