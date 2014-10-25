7cd4bbebe57b007fab65aa3c2ea3e4e96c2fb5e3
Evgeniy <sei.olympus@gmail.com>
2014-10-26 01:19:26 +0600

    first commit

---
+++ _build/data/transport.settings.php
@@ -0,0 +1,52 @@
+﻿<?php
+
+$settings = array();
+
+$tmp = array(
+	'token' => array(
+		'xtype' => 'textfield',
+		'value' => '05dd3dd84ff948fdae2bc4fb91f13e22',
+		'area' => 'modyametrika_main',
+	),
+	'counter' => array(
+		'xtype' => 'textfield',
+		'value' => '2138128',
+		'area' => 'modyametrika_main',
+	),
+	'api_url' => array(
+		'xtype' => 'textfield',
+		'value' => 'http://api-metrika.yandex.ru/',
+		'area' => 'modyametrika_main',
+	),
+	'counter_code' => array(
+		'xtype' => 'textarea',
+		'value' => '',
+		'area' => 'modyametrika_main',
+	),
+	'counter_code_show' => array(
+		'xtype' => 'combo-boolean',
+		'value' => false,
+		'area' => 'modyametrika_main',
+	),
+	'only_static' => array(
+		'xtype' => 'combo-boolean',
+		'value' => false,
+		'area' => 'modyametrika_main',
+	),
+);
+
+foreach ($tmp as $k => $v) {
+	/* @var modSystemSetting $setting */
+	$setting = $modx->newObject('modSystemSetting');
+	$setting->fromArray(array_merge(
+		array(
+			'key' => 'modyametrika_' . $k,
+			'namespace' => PKG_NAME_LOWER,
+		), $v
+	), '', true, true);
+
+	$settings[] = $setting;
+}
+
+unset($tmp);
+return $settings;
---
+++ _build/resolvers/resolve.tables.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+if ($object->xpdo) {
+	/** @var modX $modx */
+	$modx =& $object->xpdo;
+
+	switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+		case xPDOTransport::ACTION_INSTALL:
+			$modelPath = $modx->getOption('modyametrika_core_path', null, $modx->getOption('core_path') . 'components/modyametrika/') . 'model/';
+			$modx->addPackage('modyametrika', $modelPath);
+
+			$manager = $modx->getManager();
+			$objects = array(
+				// 'modYaMetrikaCounter',
+			);
+			foreach ($objects as $tmp) {
+				$manager->createObjectContainer($tmp);
+			}
+			break;
+
+		case xPDOTransport::ACTION_UPGRADE:
+			break;
+
+		case xPDOTransport::ACTION_UNINSTALL:
+			break;
+	}
+}
+return true;
