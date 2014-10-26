ae5b3ce2ad4008f9a03b0403713bb455258a856c
Evgeniy Savitskiy <sei.olympus@gmail.com>
2014-10-26 15:22:30 +0600

    Update connector.php

--- assets/components/modyametrika/connector.php
+++ assets/components/modyametrika/connector.php
@@ -1,12 +1,7 @@
 <?php
 // For debug
-<<<<<<< HEAD
  ini_set('display_errors', 1);
  ini_set('error_reporting', -1);
-=======
-// ini_set('display_errors', 1);
-// ini_set('error_reporting', -1);
->>>>>>> origin/master
 
 // require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
 if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php')) {
