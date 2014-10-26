8d66d4b92d17a53d72f37864a9d3e0f8ef40bfd5
Evgeniy <sei.olympus@gmail.com>
2014-10-26 15:18:17 +0600

    req #4

--- core/components/modyametrika/elements/plugins/plugin.modyametrika.php
+++ core/components/modyametrika/elements/plugins/plugin.modyametrika.php
@@ -5,7 +5,8 @@ switch ($modx->event->name) {
 		if ($addJs = $modx->getOption('modyametrika_counter_code')) {
 			$modx->regClientScript($addJs);
 		}
-		break;		
+		break;
+    
 	// case 'OnDocFormRender': //Будет допилено, когда будет понимание того, что тут должно быть
 		// if ($modx->getOption('modyametrika_counter_code_show') != '1') return '';
 		// $modx->modYaMetrika = $modx->getService('modyametrika','modYaMetrika',$modx->getOption('modyametrika_core_path',null,$modx->getOption('core_path').'components/modyametrika/').'model/modyametrika/',$scriptProperties);
